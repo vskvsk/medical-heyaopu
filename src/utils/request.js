f7ff7b9397cfab5fd30812a52b9e26acd1df9c3f
zh志豪 <360121615@qq.com>
2025-09-17 17:55:15 +0800

    修改打包配置

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,7 +8,8 @@ import { ACCESS_TOKEN } from '@/store/mutation-types'
 // 创建 axios 实例
 const request = axios.create({
   // API 请求的默认前缀
-  baseURL: process.env.VUE_APP_API_BASE_URL,
+  // 开发环境使用空字符串以便代理生效，生产环境使用完整URL
+  baseURL: process.env.NODE_ENV === 'development' ? '' : process.env.VUE_APP_API_BASE_URL,
   timeout: 15000 // 请求超时时间
 })
 
